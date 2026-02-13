99f9406988cc1e1c7b2e64d9d85813e9c994349e
lightlabcreation <lightlabcreation@gmail.com.com>
2026-02-13 13:01:07 +0530

    rohit

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:3000',
+  'https://worksuit-crm.kiaantechnology.com',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
